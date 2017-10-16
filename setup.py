d21636b356eb7f7d7f0c3ae450228a241e904c48
Ricardo Quiroz <32824709+rquirozr@users.noreply.github.com>
2017-10-15 19:36:00 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'rq_test1',
+    #packages = ['my_python_package'],
+    version = '0.23',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'me',
+    author_email = '',
+    url = 'https://github.com/rquirozr/Test-Package',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
