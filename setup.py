4c527f4a0ff54013990ec96bd8c7fae64c037f39
Ricardo Quiroz <32824709+rquirozr@users.noreply.github.com>
2017-10-15 19:47:17 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,57 @@
-from distutils.core import setup
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name = 'rq_test1',
-    #packages = ['my_python_package'],
-    version = '0.23',  # Ideally should be same as your GitHub release tag varsion
-    description = 'description',
-    author = 'me',
-    author_email = '',
-    url = 'https://github.com/rquirozr/Test-Package',
-    download_url = 'download link you saved',
-    keywords = ['tag1', 'tag2'],
-    classifiers = [],
+    name='rq_test1',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='1.2.0',
+
+    description='A sample Python project',
+    long_description=long_description,  #this is the
+
+    # The project's main homepage.
+    url='https://github.com/whatever/whatever',
+
+    # Author details
+    author='yourname',
+    author_email='your@address.com',
+
+    # Choose your license
+    license='MyLicense',
+
+    # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='sample setuptools development',
+
+    #packages=["MY-PACKAGE"],
+
 )
